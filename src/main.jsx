fcecf7d860121f1f41d8a9c5bdf6e6e39772c2cc
endorf8n <128092594+endorf8n@users.noreply.github.com>
2023-08-01 14:30:04 +0300

    add redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import GlobalCSS from "./styles/globalStyles";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { persistor, store } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -14,7 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <BrowserRouter>
         <GlobalCSS />
         <Provider store={store}>
-          <App />
+          <PersistGate persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </BrowserRouter>
     </ThemeProvider>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,34 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./usersSlice";
 
+const persistConfig = {
+  key: "users",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, usersReducer);
+
 export const store = configureStore({
   reducer: {
-    users: usersReducer,
+    users: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
